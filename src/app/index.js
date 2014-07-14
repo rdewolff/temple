104f399fea64994d074e20ecb7a463275c06f8b9
Romain de Wolff <rdewolff@gmail.com>
2014-07-14 20:10:22 +0200

    Trying to bind artist list and artist edit - not working yet. Create OK, edit KO! No data in edit...

--- src/app/index.js
+++ src/app/index.js
@@ -16,6 +16,49 @@ app.get('/', function(page){
   page.render('home');
 });
 
-app.get('/p/artist', function(page) {
-  page.render('artistEdit');
+app.get('/p/artist', function(page, model, params, next) {
+  var artistQuery = model.query('artist', {});
+  artistQuery.subscribe(function(err) {
+    if (err) return next(err);
+    model.ref('_page.artist', artistQuery)
+  	page.render('artistList');
+  });
 });
+
+app.get('/p/artist/:id', function(page, model, params, next) {
+  if (params.id === 'new') {
+  	console.log('new artist')
+    return page.render('artistEdit');
+  }
+  console.log('existing artist')
+  var artist = model.at('artist.' + params.id);
+  artist.subscribe(function(err) {
+    if (err) return next(err);
+    if (!artist.get()) return next();
+    //model.ref('_page.artist', artist);
+    page.render('artistEdit');
+  });
+});
+
+app.component('artistEdit', ArtistEditForm);
+function ArtistEditForm() {}
+
+ArtistEditForm.prototype.done = function() {
+	var model = this.model;
+	/* TODO validation
+	if (!model.get('artist.firstname')) {
+		var checkName = model.on('change', 'artist.firstname', function(value) {
+			if (!value) return;
+			model.del('nameError');
+			model.removeListener('change', checkName);
+		});
+		model.set('nameError', true);
+		this.firstname.focus();
+		return;
+	}
+	 */
+	if (!model.get('artist.id')) {
+		model.root.add('artist', model.get('artist'));
+	}
+	app.history.push('/p/artist');
+}
