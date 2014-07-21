05ed39a21f69c0b9d8d7c2967822d336b700097c
Romain de Wolff <rdewolff@gmail.com>
2014-07-21 16:09:16 +0200

    Created the private home page, displays the latest Objects and Artists created as well as total number of each object with link to list view

--- src/app/index.js
+++ src/app/index.js
@@ -17,8 +17,28 @@ app.loadStyles(__dirname + '/../../styles/app');
  */
 app.get('/', function(page, model, params, next) {
   
-  page.render('home');
+  var artistCount = model.query('artist', {$count: true});
+  var objectCount = model.query('collection', {$count: true});
 
+  // get the most recent objects
+  var collectionLatestQuery = model.query('collection', {$limit: 5 , $orderby: {"_m.ctime": -1}}); 
+  var artistLatestQuery = model.query('artist', {$limit: 5 , $orderby: {"_m.ctime": -1}}); 
+
+  model.subscribe(artistCount, objectCount, collectionLatestQuery, artistLatestQuery, function onCount(err, next) {
+    if (err) return next(err);
+
+    // livedb-mongo returns count queries as an `extraSegment`  https://github.com/share/livedb-mongo/blob/v0.3.2/mongo.js#L227
+    // unlike the other supported metaOperators                 https://github.com/share/livedb-mongo/blob/v0.3.2/mongo.js#L7
+    // and 0.6 now allows references to `extraSegments`         https://github.com/codeparty/racer/blob/v0.6.0-alpha3/lib/Model/Query.js#L494
+    artistCount.refExtra('_page.artistCount');
+    objectCount.refExtra('_page.objectCount');
+
+    collectionLatestQuery.ref('_page.collection');
+    artistLatestQuery.ref('_page.artist');
+
+    // Async functions must always run a callback. Always.
+    page.render('home');
+  });
 });
 
 app.use(require('./artist'));
