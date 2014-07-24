c150adefa61e61217a7d096c8fcc9e9941a4e232
Romain de Wolff <rdewolff@gmail.com>
2014-07-24 17:46:07 +0200

    Added API route to server collection to JSON. Format : hostname/api/v1/<collection>

--- server/routes.js
+++ server/routes.js
@@ -2,10 +2,18 @@ var express = require('express');
 
 var router = express.Router();
 
-router.post('/api/v1/item', function(req, res){
+// get the collection passed in param
+router.get('/api/v1/:collection', function(req, res){
+  
+  var collection = req.params.collection
   var model = req.getModel();
-  //...
-  res.json(true);
+  
+  var artistQuery = model.query(collection, {});
+	artistQuery.subscribe(function(err) {
+  	if (err) return next(err);
+  	res.json(artistQuery.get());
+	});
+
 });
 
 module.exports = router;
