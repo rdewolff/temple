4b7f5a5ff443c3364d213b7cb31b4aac9023f707
Romain de Wolff <rdewolff@gmail.com>
2014-07-27 10:23:29 +0200

    New public home in progress

--- src/public/index.js
+++ src/public/index.js
@@ -17,3 +17,17 @@ app.loadStyles(__dirname + '/../../styles/public');
 app.get('/', function(page, model, params, next) {
   page.render();
 });
+
+
+app.proto.create = function(model) {
+
+  /**
+   * Import scripts
+   */
+  // application wide jquery
+  require('../../public/vendor/jquery-1.9.1.min'); // the JQuery version from Derby example todos WORKS
+  // NOT WORKING : require('../../public/components/jquery/dist/jquery.min'); // the Bower JQuery version does NOT work
+
+  require('../../public/components/bootstrap/dist/js/bootstrap.min');
+  
+}
