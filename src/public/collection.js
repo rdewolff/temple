a5cffdc5a9168fbf766f28223b118194aabcbebd
Romain de Wolff <rdewolff@gmail.com>
2014-08-02 18:58:08 +0200

    Trying filter (2) - in progress

--- src/public/collection.js
+++ src/public/collection.js
@@ -7,13 +7,15 @@ module.exports = function(app, options) {
   app.on('model', function(model) {
     model.fn('filter', function(item, key, obj) {
       // TODO: this is not working with live binding : model.get('_page.filterChoice'); // "Red";
+      console.log(model.get('#root._page.collection'));
       return item.domain === 'Red';
     });
   });
 
-  app.get('/collection/f/red', function(page, model, params, next) {
+  app.get('/collection/filter/:red', function(page, model, params, next) {
     model.subscribe('collection', function(err){
       if (err) return next(err);
+
       var filter = model.filter('collection', 'filter');
       filter.ref('_page.collection');
       model.set('_page.filter', [{content: 'Red'}, {content: 'Orange'}, {content: 'Purple'}]);
