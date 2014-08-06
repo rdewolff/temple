05cb59c3b89a655437325eca3fea19371a8ba888
Romain de Wolff <rdewolff@gmail.com>
2014-08-06 11:11:06 +0200

    Public Collection order and filter working and cleaned

--- src/public/collection.js
+++ src/public/collection.js
@@ -3,48 +3,29 @@
  */
 
 module.exports = function(app, options) {
-  /*
-  app.on('model', function(model) {
-    // TODO: this is not working with live binding : model.get('_page.filterChoice'); // "Red";
-    model.fn('filter-red', function(item, key, obj) {
-      return item.domain === 'Red';
-    });
-    model.fn('filter-orange', function(item, key, obj) {
-      return item.domain === 'Orange';
-    });
-    model.fn('filter-purple', function(item, key, obj) {
-      return item.domain === 'Purple';
-    });
-  });
-
-  app.get('/collection/filter/:option', function(page, model, params, next) {
-    model.subscribe('collection', function(err){
-      if (err) return next(err);
-      var filter = model.filter('collection', 'filter-'+params.option);
-      filter.ref('_page.collection');
-      model.set('_page.filter', [{content: 'Red'}, {content: 'Orange'}, {content: 'Purple'}]); // filter options
-      page.render('collectionList');
-    });
-  });*/
 
   app.get('/collection', function(page, model, params, next) {
-    var collection = model.query('collection', {
+    var query = {
       domain: params.query.filter,
       $or: [
         {publish: 'Public'},
         {publish: 'Highlight'}
       ],
-      $orderby: [
-        {'yearFrom': 1} // FIXME: how to use `params.query.sort` in here ?? Cf. http://stackoverflow.com/questions/2738298/using-a-variable-as-identifier-in-a-json-array
-      ]
-    });
+      $orderby: [{}]
+    };
+    query.$orderby[0][params.query.sort] = 1; // use the parameters for the order by
+    // debug : console.log(query.$orderby);
+
+    var collection = model.query('collection', query);
     model.subscribe(collection, function(err) {
       if (err) return next(err);
 
       collection.ref('_page.collection');
+
+      // FIXME: stored in the database and editable in the admin area
       model.set('_page.filter', [{content: 'Red'}, {content: 'Orange'}, {content: 'Purple'}]);
       model.set('_page.order', [{content: 'Title'}]);
-      // model.filter()
+
       page.render('collectionList');
     });
   });
