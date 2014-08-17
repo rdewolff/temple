085c44e2f38880187ab93f41e5ffe529be02f784
Romain de Wolff <rdewolff@gmail.com>
2014-08-17 20:42:06 +0200

    Added admin views

--- src/app/admin.js
+++ src/app/admin.js
@@ -72,7 +72,7 @@ module.exports = function(app, options) {
       model.ref('_page.adminFields', adminFields);
       page.render('adminFields');
     });
-    
+
   });
 
   app.component('adminFields', AdminFieldsForm);
@@ -89,8 +89,51 @@ module.exports = function(app, options) {
   AdminFieldsForm.prototype.fieldsDuplicate = function (id) {
     var duplicateField = this.model.root.get('adminFields.'+id);
     duplicateField['id'] = this.model.id(); // new id
-    delete duplicateField['__proto__'];
+    delete duplicateField['__proto__']; // clean
     this.model.root.add('adminFields', duplicateField);
   }
+
+  /**
+   * VIEWS
+   */
+  app.get('/p/admin/views', function(page, model, params, next){
+
+    var adminViews = model.query('adminViews', {});
+    model.subscribe(adminViews, function(err, next) {
+      // if no data, add an example
+      if (!adminViews.get().length) {
+        model.add('adminViews', {
+          name: 'name',
+          data: 'data',
+          viewType: 'view type',
+          location: 'location',
+          comment: 'comment'
+        });
+      }
+      model.ref('_page.adminViews', adminViews);
+      page.render('adminViews');
+    });
+
+  });
+
+
+  app.component('adminViews', adminViewsForm);
+  function adminViewsForm() {}
+
+  adminViewsForm.prototype.viewsAdd = function () {
+    this.model.root.add('adminViews', this.model.del('_page.adminViewNew'));
+  }
+
+  adminViewsForm.prototype.viewsDelete = function (id) {
+    this.model.root.del('adminViews.'+id);
+  }
+
+  adminViewsForm.prototype.viewsDuplicate = function (id) {
+    var duplicateView = this.model.root.get('adminViews.'+id);
+    duplicateView['id'] = this.model.id(); // new id
+    delete duplicateView['__proto__']; // clean
+    this.model.root.add('adminViews', duplicateView);
+  }
+
 
 }
