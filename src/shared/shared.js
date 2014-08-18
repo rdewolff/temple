01c5f9fb64f341f070060e834d5931dde14f3b36
Romain de Wolff <rdewolff@gmail.com>
2014-08-18 18:08:30 +0200

    Ops - uncommented

--- src/shared/shared.js
+++ src/shared/shared.js
@@ -6,7 +6,7 @@ module.exports = function(app, options) {
       // FIXME: path /a/bc should not return true when path /a/b give. Full word matching should be implemented.
       // console.log(currentUrl, linkUrl, (currentUrl.split('/')).indexOf(linkUrl));
       // return ((currentUrl.split('/')).indexOf(linkUrl.split('/')) > -1);
-      // return (currentUrl.indexOf(linkUrl) > -1);
+      return (currentUrl.indexOf(linkUrl) > -1);
     }
 
 }
