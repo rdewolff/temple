b416baf464377f4e23c8816b2072c17336ed8e14
Romain de Wolff <rdewolff@gmail.com>
2014-08-18 17:58:50 +0200

    Added fixme note for containUrl() function

--- src/shared/shared.js
+++ src/shared/shared.js
@@ -3,7 +3,10 @@ module.exports = function(app, options) {
     // TODO: move to component?
     // check if the link is part of the URL
     app.proto.containUrl = function(currentUrl, linkUrl) {
-      return (currentUrl.indexOf(linkUrl) > -1);
+      // FIXME: path /a/bc should not return true when path /a/b give. Full word matching should be implemented.
+      // console.log(currentUrl, linkUrl, (currentUrl.split('/')).indexOf(linkUrl));
+      // return ((currentUrl.split('/')).indexOf(linkUrl.split('/')) > -1);
+      // return (currentUrl.indexOf(linkUrl) > -1);
     }
 
 }
