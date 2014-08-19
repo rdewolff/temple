b5caa2f6c2690430a6f51d99ba6a9dec19c2bdff
Romain de Wolff <rdewolff@gmail.com>
2014-08-19 13:30:54 +0200

    Added info for the containUrl() function. Added the regex to cut by word

--- src/shared/shared.js
+++ src/shared/shared.js
@@ -6,6 +6,7 @@ module.exports = function(app, options) {
       // FIXME: path /a/bc should not return true when path /a/b give. Full word matching should be implemented.
       // console.log(currentUrl, linkUrl, (currentUrl.split('/')).indexOf(linkUrl));
       // return ((currentUrl.split('/')).indexOf(linkUrl.split('/')) > -1);
+      // regular expression to match a word : \b([a-zA-Z0-9]*)\b
       return (currentUrl.indexOf(linkUrl) > -1);
     }
 
